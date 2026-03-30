ac6eafb5b08ec636aa9041fa0f8cbfbb5b67ebd1
Danko <pirgan@gmail.com>
2026-03-30 14:54:38 +0200

    debug: expose boot error in api/index.js to diagnose FUNCTION_INVOCATION_FAILED

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
 import 'dotenv/config';
-import connectDB from '../server/src/config/db.js';
-import app from '../server/src/app.js';
 
+let app;
 let connected = false;
 
-export default async function handler(req, res) {
+async function init() {
+  const [{ default: connectDB }, { default: expressApp }] = await Promise.all([
+    import('../server/src/config/db.js'),
+    import('../server/src/app.js'),
+  ]);
   if (!connected) {
     await connectDB();
     connected = true;
   }
-  return app(req, res);
+  app = expressApp;
+}
+
+export default async function handler(req, res) {
+  try {
+    if (!app) await init();
+    return app(req, res);
+  } catch (err) {
+    console.error('[api/index] boot error:', err);
+    return res.status(500).json({ error: err.message, stack: err.stack?.split('\n').slice(0, 5) });
+  }
 }
